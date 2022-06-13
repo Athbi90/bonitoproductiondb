6fb5783e594b3a9b86b08a7e467f7bb0def64ad4
athbi90 <athbi90@gmail.com>
2022-06-13 21:08:50 +0300

    fix sequelize in app.js

--- app.js
+++ app.js
@@ -7,15 +7,14 @@ const { Umzug, SequelizeStorage } = require("umzug");
 const Sequelize = require("sequelize");
 const fs = require("fs");
 
-const sequelize = new Sequelize(
-  `${process.env.DB_NAME}`,
-  `${process.env.DB_USERNAME}`,
-  `${process.env.DB_PASSWORD}`,
-  {
-    host: `${process.env.DB_HOST}`,
-    dialect: "postgres",
-  }
-);
+const sequelize = new Sequelize(process.env[config.use_env_variable], {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: true,
+    useUTC: false,
+  },
+  timezone: "+03:00",
+});
 
 const umzug = new Umzug({
   migrations: {
