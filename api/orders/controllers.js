1f2a74e7306ffff150410377bdc9f6ecd2b09a12
athbi90 <athbi90@gmail.com>
2022-06-14 23:54:25 +0300

    Add decimals for amount

--- api/orders/controllers.js
+++ api/orders/controllers.js
@@ -272,7 +272,13 @@ exports.tapPost = async (req, res, next) => {
     console.log("Posting from Tap");
     console.log("BODY", req.body);
 
-    const toBeHashedString = `x_id${req.body.id}x_amount${req.body.amount}x_currency${req.body.currency}x_gateway_reference${req.body.reference.gateway}x_payment_reference${req.body.reference.payment}x_status${req.body.status}x_created${req.body.transaction.created}`;
+    const toBeHashedString = `x_id${
+      req.body.id
+    }x_amount${req.body.amount.toFixed(3)}x_currency${
+      req.body.currency
+    }x_gateway_reference${req.body.reference.gateway}x_payment_reference${
+      req.body.reference.payment
+    }x_status${req.body.status}x_created${req.body.transaction.created}`;
 
     const hash = CryptoJS.HmacSHA256(
       toBeHashedString,
